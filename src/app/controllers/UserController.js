586087a73f00fdfd1cdb9d27597142644d69d72a
Ricardo Luiz Vieira <ricardoluizv@gmail.com>
2019-09-22 20:57:03 -0300

    fix: User authentication with JWT

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
@@ -11,13 +12,47 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(3),
+      password: Yup.string()
+        .min(3)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, oldPassword } = req.body;
-    // if (oldPassword && !(await user.checkPassword(oldPassword))) {
-    //   return res.status(401).json({ error: 'Password dos not match' });
-    // }
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password dos not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
     return res.json({
+      id,
+      name,
       email,
-      oldPassword,
+      provider,
     });
   }
 }
