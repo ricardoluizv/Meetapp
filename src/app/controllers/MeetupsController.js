91b448577abc64a549a18bc61b8321c418e32e33
Ricardo Luiz Vieira <ricardoluizv@gmail.com>
2019-10-01 21:05:19 -0300

    chore: Update meetups

--- src/app/controllers/MeetupsController.js
+++ src/app/controllers/MeetupsController.js
@@ -1,6 +1,22 @@
+import {
+  // startOfDay,
+  // endOfDay,
+  parseISO,
+  isAfter,
+  // format,
+  startOfHour,
+  endOfHour,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import { da } from 'date-fns/locale';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
+  /**
+   * Cadastra um novo evento
+   * @param {*} req
+   * @param {*} res
+   */
   async store(req, res) {
     const {
       avatar_id,
@@ -12,8 +28,27 @@ class MeetupController {
       date,
     } = req.body;
 
-    console.log(`avatarID: ${avatar_id}`);
-    console.log(`userID: ${req.userId}`);
+    const parsedDate = parseISO(date); // converte a data GMT -3:00 para a hora corrreta
+
+    if (!isAfter(parsedDate, new Date())) {
+      return res.status(401).json({
+        error: 'Date provided must bigger that current date',
+      });
+    }
+
+    const checkMeetupExists = await Meetup.findOne({
+      where: {
+        date: {
+          [Op.between]: [startOfHour(parsedDate), endOfHour(parsedDate)],
+        },
+      },
+    });
+
+    if (checkMeetupExists) {
+      return res.status(401).json({ error: 'Date already exists' });
+    }
+
+    // return res.json({ ok: 'OK' });
 
     const meetup = await Meetup.create({
       user_id: req.userId,
@@ -27,6 +62,90 @@ class MeetupController {
     });
 
     return res.json(meetup);
+  }
+
+  /**
+   *Atualiza o meetup, somente se o usuário logado for o organizador do evento
+   * @param {* Dados para atualizar o meetup} req
+   * @param {* Se tudo ocorrer bem, resultará o json dos dados} res
+   */
+  async update(req, res) {
+    const {
+      id,
+      avatar_id,
+      title,
+      description,
+      address,
+      number,
+      uf,
+      date,
+    } = req.body;
+
+    // #region Validate fields
+
+    let parsedDate = parseISO(date); // converte a data GMT -3:00 para a hora corrreta
+
+    if (!isAfter(parsedDate, new Date())) {
+      return res.status(401).json({
+        error: 'Date provided must bigger that current date',
+      });
+    }
+
+    const checkMeetupExists = await Meetup.findOne({
+      where: {
+        date: {
+          [Op.between]: [startOfHour(parsedDate), endOfHour(parsedDate)],
+        },
+        id: {
+          [Op.ne]: [id],
+        },
+      },
+    });
+
+    if (checkMeetupExists) {
+      return res.status(401).json({ error: 'Date already exists' });
+    }
+
+    // #endregion
+
+    // #region Verifica se o usuáro logado é o arganizador do envento
+
+    const checkMeetup = await Meetup.findOne({
+      where: { id, user_id: req.userId },
+    });
+
+    if (!checkMeetup) {
+      return res.json({
+        error: 'The user logged is not the organizer this event.',
+      });
+    }
+
+    // Verifica se o evento já aconteceu
+    if (checkMeetup.date <= date) {
+      return res.json({
+        error: `The date provided must be begger than ${checkMeetup.date}`,
+      });
+    }
+
+    parsedDate = parseISO(checkMeetup.date); // converte a data GMT -3:00 para a hora corrreta
+    const parsedDateNew = parseISO(date);
+
+    if (!isAfter(parsedDateNew, parsedDate)) {
+      return res.status(401).json({
+        error: 'Date provided must bigger than stored date',
+      });
+    }
+
+    checkMeetup.title = title;
+    checkMeetup.description = description;
+    checkMeetup.address = address;
+    checkMeetup.number = number;
+    checkMeetup.uf = uf;
+    checkMeetup.date = date;
+    checkMeetup.avatar_id = avatar_id;
+    checkMeetup.save();
+
+    return res.json(checkMeetup);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -20,5 +20,6 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/meetups', MeetupsController.store);
+routes.put('/meetups', MeetupsController.update);
 
 export default routes;
